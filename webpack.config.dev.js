317904c74949a4b9c07cd3753dd8f381a3b5f605
Zach Smith <z4smith@ucsd.edu>
2018-04-03 14:13:17 -0700

    Adds starter configurations for linting (eslint/airbnb), transpiling (babel), and bundling (webpack)

---
+++ webpack.config.dev.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader',
+        ]
+      },
+
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './src',
+    hot: true
+  }
+};
