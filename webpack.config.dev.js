03bc7a7daad4a1e808020bd3b6650657ef817351
Zach Smith <z4smith@ucsd.edu>
2018-04-18 10:41:27 -0700

    Adds additional configuration to webpack.config.dev.js for font/image handling with fileloader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,11 +8,13 @@ module.exports = {
   ],
   module: {
     rules: [
+      // load js/react through babel-loader
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      // lint js files with eslint
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -21,15 +23,33 @@ module.exports = {
           'eslint-loader',
         ],
       },
-
+      // load css via style-loader and css-loader
       {
         test: /\.css$/,
-        loader: 'style!css',
+        use: ['style-loader', 'css-loader'],
+      },
+      // load files via file-loader
+      {
+        test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+        use: 'file-loader?name=[name].[ext]?[hash]',
+      },
+      // font extraction via file/url loader
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
       },
     ],
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
+    modules: [
+      path.resolve('./src'),
+      path.resolve('./node_modules'),
+    ],
   },
   output: {
     path: path.join(__dirname, 'dist'),
@@ -37,6 +57,7 @@ module.exports = {
     publicPath: '/',
   },
   plugins: [
+    // hot refresh
     new webpack.HotModuleReplacementPlugin(),
   ],
   devServer: {
