a00b9bafd4a911742a9540d6a66957218622078d
Zach Smith <z4smith@ucsd.edu>
2018-04-19 15:02:32 -0700

    Moved landingpage layout to layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Menu, Container, Card, Icon } from 'semantic-ui-react';
 import Header from './Components/Layout/Header';
+import LandingPage from './Components/Layout/LandingPage';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeItem: 'home',
-    };
-  }
-
-  handleItemClick() {
-    // TODO
-
-    this.setState();
-  }
-
 
   render() {
-    const { activeItem } = this.state;
-    const items = [
-      {
-        header: 'Specimen',
-        description: 'Specimen and Aliquot Management',
-        color: 'red',
-      },
-      {
-        header: 'Shipping',
-        description: 'Shipping and Recieving',
-        color: 'blue',
-      },
-      {
-        header: 'Query',
-        description: 'Search Tool',
-        color: 'green',
-      },
-      {
-        header: 'Storage Setup',
-        description: 'Set up Storage',
-        color: 'orange',
-      },
-      {
-        header: 'Action Items',
-        description: 'Items pending action',
-        color: 'teal',
-      },
-      {
-        header: 'Patient Scheduling',
-        description: 'Schedule Patients',
-        color: 'violet',
-      },
-    ];
-
     return (
       <div>
         <Header />
-        <Container>
-          <Card.Group centered itemsPerRow={3} items={items} />
-        </Container>
+        <LandingPage />
       </div>
     );
   }
---
+++ src/Components/Layout/LandingPage.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Container, Card, Icon } from 'semantic-ui-react';
+
+export default class LandingPage extends React.Component {
+  render() {
+    const items = [
+      {
+        header: 'Specimen',
+        description: 'Specimen and Aliquot Management',
+        color: 'red',
+      },
+      {
+        header: 'Shipping',
+        description: 'Shipping and Recieving',
+        color: 'blue',
+      },
+      {
+        header: 'Query',
+        description: 'Search Tool',
+        color: 'green',
+      },
+      {
+        header: 'Storage Setup',
+        description: 'Set up Storage',
+        color: 'orange',
+      },
+      {
+        header: 'Action Items',
+        description: 'Items pending action',
+        color: 'teal',
+      },
+      {
+        header: 'Patient Scheduling',
+        description: 'Schedule Patients',
+        color: 'violet',
+      },
+    ];
+
+    return (
+      <Container>
+        <Card.Group centered itemsPerRow={3} items={items} />
+      </Container>
+    );
+  }
+}
+
