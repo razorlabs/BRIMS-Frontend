e7892bc3dac29e70bc9c6fba1537cc8a0fc96497
Zach Smith <z4smith@ucsd.edu>
2018-04-24 10:55:45 -0700

    Adds placeholders for additional views and modifys App.jsx for viewing (routing not yet integrated)

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom';
 import Header from './Components/Layout/Header';
 import LandingPage from './Components/Layout/LandingPage';
 import Specimen from './Components/Layout/Specimen';
+import Shipping from './Components/Layout/Shipping';
+import PatientScheduling from './Components/Layout/PatientScheduling';
+import Query from './Components/Layout/Query';
+import StorageSetup from './Components/Layout/StorageSetup';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <LandingPage />
+        {/*
+         <LandingPage />
+        */}
         {/*
          <Specimen />
         */}
+        {/*
+        <Shipping />
+        */}
+        {/*
+        <PatientScheduling />
+        */}
+        {/*
+        <Query />
+        */}
+        <StorageSetup />
+
       </div>
     );
   }
---
+++ src/Components/Layout/Query.jsx
@@ -0,0 +1,14 @@
+import { Segment } from 'semantic-ui-react';
+import React from 'react';
+import Header from './Header';
+
+export default class Query extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
