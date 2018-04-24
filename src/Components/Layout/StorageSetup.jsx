bab433387595f14b9a0fc84407dae42dc446e44f
Zach Smith <z4smith@ucsd.edu>
2018-04-24 15:11:00 -0700

    Adds Storage Setup demo view

--- src/Components/Layout/StorageSetup.jsx
+++ src/Components/Layout/StorageSetup.jsx
@@ -1,12 +1,89 @@
-import { Segment } from 'semantic-ui-react';
+import { Table, Button, Segment, Container, List, Icon } from 'semantic-ui-react';
 import React from 'react';
 import Header from './Header';
+
+
+class BoxGrid extends React.Component {
+  // Cells are intentionally left un-selfclosing for rapid-demo
+  render() {
+    return (
+      <Table textAlign="center" fixed celled striped definition size="small">
+        <Table.Footer fullWidth>
+          <Table.Row>
+            <Table.HeaderCell />
+            <Table.HeaderCell> A </Table.HeaderCell>
+            <Table.HeaderCell> B </Table.HeaderCell>
+            <Table.HeaderCell> C </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>1</Table.Cell>
+            <Table.Cell>222-22a</Table.Cell>
+            <Table.Cell>222-22b</Table.Cell>
+            <Table.Cell></Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>2</Table.Cell>
+            <Table.Cell></Table.Cell>
+            <Table.Cell></Table.Cell>
+            <Table.Cell></Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>3</Table.Cell>
+            <Table.Cell>223-abc</Table.Cell>
+            <Table.Cell></Table.Cell>
+            <Table.Cell>222-bca</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    );
+  }
+}
 
 export default class StorageSetup extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Segment vertical clearing="true">
+          <Button> Manage Storage </Button>
+        </Segment>
+        <Container>
+          <Segment>
+            <List divided="true">
+              <List.Item>
+                <List.Icon name="snowflake outline" />
+                <List.Content>
+                  <List.Header>Fridge 001 </List.Header>
+                  <List.Description>TSX Series AB31</List.Description>
+                  <List.List>
+                    <List.Item>
+                      <List.Icon name="dropbox" />
+                      <List.Content>
+                        <List.Header>Box CCTG_001 </List.Header>
+                        <List.Description>Dry Blood Spot Storage Box </List.Description>
+                        <List.Item>
+                          <BoxGrid />
+                        </List.Item>
+                      </List.Content>
+                    </List.Item>
+                    <List.Item>
+                      <List.Icon name="dropbox" />
+                      <List.Content>
+                        <List.Header>Box CCTG_002 </List.Header>
+                        <List.Description>Dry Blood Spot Storage Box </List.Description>
+                        <List.Item>
+                          <BoxGrid />
+                        </List.Item>
+                      </List.Content>
+                    </List.Item>
+                  </List.List>
+                </List.Content>
+              </List.Item>
+            </List>
+          </Segment>
+        </Container>
       </div>
     );
   }
