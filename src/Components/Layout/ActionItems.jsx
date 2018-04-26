8565044c1404fbba96a07ff111d4f8dc79453973
Zach Smith <z4smith@ucsd.edu>
2018-04-26 10:52:54 -0700

    Adds actionitems boilerplate

---
+++ src/Components/Layout/ActionItems.jsx
@@ -0,0 +1,14 @@
+import { Segment } from 'semantic-ui-react';
+import React from 'react';
+import Header from './Header';
+
+export default class ActionItems extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
