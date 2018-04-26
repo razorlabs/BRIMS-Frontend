ef42bc951bac7d28f321c7b9f5c84316b67db98d
Zach Smith <z4smith@ucsd.edu>
2018-04-26 10:23:04 -0700

    Hooks routing into cards on landing page

--- src/Components/Layout/LandingPage.jsx
+++ src/Components/Layout/LandingPage.jsx
@@ -1,4 +1,5 @@
 import { Container, Card, Icon } from 'semantic-ui-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React from 'react';
 import Header from './Header';
 
@@ -9,31 +10,37 @@ export default class LandingPage extends React.Component {
         header: 'Specimen',
         description: 'Specimen and Aliquot Management',
         color: 'red',
+        href: '/specimen',
       },
       {
         header: 'Shipping',
         description: 'Shipping and Recieving',
         color: 'blue',
+        href: '/shipping',
       },
       {
         header: 'Query',
         description: 'Search Tool',
         color: 'green',
+        href: '/query',
       },
       {
         header: 'Storage Setup',
         description: 'Set up Storage',
         color: 'orange',
+        href: '/storage',
       },
       {
         header: 'Action Items',
         description: 'Items pending action',
         color: 'teal',
+        href: '/action',
       },
       {
         header: 'Patient Scheduling',
         description: 'Schedule Patients',
         color: 'violet',
+        href: '/schedule',
       },
     ];
 
