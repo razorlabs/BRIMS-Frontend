d54e10cbbb4e2f55f14be3f5b9af1cd6f0c6038f
Zach Smith <z4smith@ucsd.edu>
2018-04-30 15:01:54 -0700

    Added rough outline of Patientscheduling view

--- src/Components/Layout/PatientScheduling.jsx
+++ src/Components/Layout/PatientScheduling.jsx
@@ -1,12 +1,64 @@
-import { Segment } from 'semantic-ui-react';
+import { Label, Table, Button, Container, Segment } from 'semantic-ui-react';
 import React from 'react';
 import Header from './Header';
+
+const VisitSpecimenTable = () => (
+  <Table celled definition>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell />
+        <Table.HeaderCell>
+          Week 0
+        </Table.HeaderCell>
+        <Table.HeaderCell>
+          Week 2
+        </Table.HeaderCell>
+        <Table.HeaderCell>
+          Week 4
+        </Table.HeaderCell>
+        <Table.HeaderCell>
+          Week 8
+        </Table.HeaderCell>
+        <Table.HeaderCell>
+          Week 16
+        </Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>Specimen</Table.Cell>
+        <Table.Cell>
+          <Label as="a" horizontal>Dry Blood Spot</Label>
+          <Label as="a" horizontal>Blood</Label>
+          <Label as="a" horizontal>Urine</Label>
+        </Table.Cell>
+        <Table.Cell>
+          <Label as="a" horizontal>Swab</Label>
+        </Table.Cell>
+        <Table.Cell>
+          <Label as="a" horizontal>Blood</Label>
+        </Table.Cell>
+        <Table.Cell>
+          <Label as="a" horizontal>Blood</Label>
+        </Table.Cell>
+        <Table.Cell>
+          <Label as="a" horizontal>Plasma</Label>
+        </Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
+);
 
 export default class PatientScheduling extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Container>
+          <Button>Add Visit</Button>
+          <Button>Add Specimen Draw</Button>
+          <VisitSpecimenTable />
+        </Container>
       </div>
     );
   }
