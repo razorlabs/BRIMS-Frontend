ce7b497bf726e17da25c3d743b6e7816edfc004d
Zach Smith <zsmith@lims_demo.younglabs.ucsd.edu>
2018-06-13 12:56:47 -0700

    Fixes react-router css routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './Components/Layout/Header';
+import React, { Component } from 'react';
 import LandingPage from './Components/Layout/LandingPage';
 import Specimen from './Components/Layout/Specimen';
 import Shipping from './Components/Layout/Shipping';
@@ -9,26 +6,32 @@ import PatientScheduling from './Components/Layout/PatientScheduling';
 import Query from './Components/Layout/Query';
 import StorageSetup from './Components/Layout/StorageSetup';
 import ActionItems from './Components/Layout/ActionItems';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/specimen" component={Specimen} />
-          <Route path="/shipping" component={Shipping} />
-          <Route path="/schedule" component={PatientScheduling} />
-          <Route path="/query" component={Query} />
-          <Route path="/storage" component={StorageSetup} />
-          <Route path="/action" component={ActionItems} />
-        </div>
-      </BrowserRouter>
+      <Router> 
+      	<div className="App">
+       	 <div>
+           {/* Switch will cause only the first matching route to be displayed
+               Note, making the following an exact path will break any backend 
+               javascript runners that attempt to host via */}
+            
+           <Switch>
+             <Route path={`${process.env.PUBLIC_URL}/lims/specimen`} component={Specimen} />
+             <Route path={`${process.env.PUBLIC_URL}/lims/shipping`} component={Shipping} />
+             <Route path={`${process.env.PUBLIC_URL}/lims/schedule`} component={PatientScheduling} />
+             <Route path={`${process.env.PUBLIC_URL}/lims/query`} component={Query} />
+             <Route path={`${process.env.PUBLIC_URL}/lims/storage`} component={StorageSetup} />
+             <Route path={`${process.env.PUBLIC_URL}/lims/action`} component={ActionItems} />
+             <Route exact path={`${process.env.PUBLIC_URL}/lims`} component={LandingPage} />
+           </Switch>
+       	 </div>
+      	</div>
+      </Router> 
     );
   }
 }
 
-
-ReactDOM.render(<App />, document.getElementById('app'));
-
-module.hot.accept();
+export default App;
