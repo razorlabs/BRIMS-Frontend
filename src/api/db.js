e59522f81659935c97e20b02cf2176b37330da42
Zach Smith <z4smith@ucsd.edu>
2018-06-14 18:35:26 -0700

    Removes underscors from db.js allowing it to work with json-graphql-server

--- src/api/db.js
+++ src/api/db.js
@@ -1,6 +1,21 @@
 module.exports = {
-  users: [
-    { id: 1, name: 'John Doe' },
+  patients: [
+    {
+      externalid: '501-42-444',
+      specimen: {
+        id: '4',
+        type: 'Dried Blood Spot',
+        collectdate: '2018-04-26',
+        collecttime: '23:20',
+        volume: '10ml',
+        aliquot: {
+          id: '5',
+          visit: 'week1',
+          collecttime: '2018-04-04',
+          volume: '5ml',
+          notes: 'some example notes',
+        },
+      },
+    },
   ],
 };
-
