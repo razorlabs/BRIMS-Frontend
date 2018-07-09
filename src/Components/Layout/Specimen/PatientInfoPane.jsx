a264ef2365d75c1804b0c0752c73da0a0dfdf9a1
Zach Smith <z4smith@ucsd.edu>
2018-07-09 14:25:47 -0700

    Adjusts Patient Info Pane to center

--- src/Components/Layout/Specimen/PatientInfoPane.jsx
+++ src/Components/Layout/Specimen/PatientInfoPane.jsx
@@ -13,49 +13,47 @@ class PatientInfoPane extends React.Component {
       return <p> Error! </p>;
     }
     return (
-      <div>
-        <Container>
-          <Segment padded>
-            <Label size="large" attached="top left">
-              Patient Info Pane
-            </Label>
-            <Container>
-              <Segment.Group horizontal>
-                <Segment>
-                  <List>
-                    <List.Item>
-                      <List.Icon name="address card" />
-                      <List.Content>SSID: {`${this.props.data.Patient.id}`}</List.Content>
-                    </List.Item>
-                    <List.Item>
-                      <List.Icon name="address book" />
-                      <List.Content>PID: {`${this.props.data.Patient.pid}`}</List.Content>
-                    </List.Item>
-                    <List.Item>
-                      <List.Icon name="address book outline" />
-                      <List.Content>External ID: {`${this.props.data.Patient.externalid}`}</List.Content>
-                    </List.Item>
-                  </List>
-                </Segment>
-                <Segment >
-                  <List>
-                    <List.Item>
-                      <List.Icon name="exclamation" />
-                      <List.Content>Next draw: Week 8</List.Content>
-                    </List.Item>
-                    <List.Item>
-                      <List.Icon name="eyedropper" />
-                      <List.Content>Blood <Icon color="blue" name="plus circle" /> </List.Content>
-                      <List.Icon name="eyedropper" />
-                      <List.Content>Urine <Icon color="blue" name="plus circle" /> </List.Content>
-                    </List.Item>
-                  </List>
-                </Segment>
-              </Segment.Group>
-            </Container>
-          </Segment>
-        </Container>
-      </div>
+      <Container>
+        <Segment>
+          <Label size="large" attached="top left">
+            Patient Info Pane
+          </Label>
+          <Container>
+            <Segment.Group horizontal>
+              <Segment>
+                <List>
+                  <List.Item>
+                    <List.Icon name="address card" />
+                    <List.Content>SSID: {`${this.props.data.Patient.id}`}</List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <List.Icon name="address book" />
+                    <List.Content>PID: {`${this.props.data.Patient.pid}`}</List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <List.Icon name="address book outline" />
+                    <List.Content>External ID: {`${this.props.data.Patient.externalid}`}</List.Content>
+                  </List.Item>
+                </List>
+              </Segment>
+              <Segment >
+                <List>
+                  <List.Item>
+                    <List.Icon name="exclamation" />
+                    <List.Content>Next draw: Week 8</List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <List.Icon name="eyedropper" />
+                    <List.Content>Blood <Icon color="blue" name="plus circle" /> </List.Content>
+                    <List.Icon name="eyedropper" />
+                    <List.Content>Urine <Icon color="blue" name="plus circle" /> </List.Content>
+                  </List.Item>
+                </List>
+              </Segment>
+            </Segment.Group>
+          </Container>
+        </Segment>
+      </Container>
     );
   }
 }
