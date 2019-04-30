3ae2930b3096ec744039f897c513d4f83ac49bfa
Zach Smith <z4smith@ucsd.edu>
2019-04-30 15:20:41 -0700

    Adds conditional logic for error message and redirect on login

--- src/Components/Layout/Auth.jsx
+++ src/Components/Layout/Auth.jsx
@@ -1,34 +1,73 @@
-import { Container, Button, Form, Grid, Header, Segment } from 'semantic-ui-react';
+import { Container, Button, Form, Grid, Header, Segment, Message } from 'semantic-ui-react';
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+function errorMessage(Component) {
+  return function withError({ loginerror }) {
+    if (loginerror) {
+      return
+    }
+    return <div />;
+  };
+}
 
 class Login extends React.Component {
-    state = { username: '', password: '' };
+    state = {
+      username: '',
+      password: '',
+      loggedin: false,
+      loginerror: false,
+    };
 
-    onChange = event => this.setState({ [event.target.name]: event.target.value })
+    onChange = event => this.setState({ [event.target.name]: event.target.value });
 
     handleSubmit = () => {
       this.props.mutate({
-        variables: { username: this.state.username, password: this.state.password },
+        variables: {
+          username: this.state.username,
+          password: this.state.password,
+        },
       })
-        .then(({ data }) => {
-          console.log('got data', data);
-        }).catch((error) => {
-          console.log('there was an error sending the query', error);
-        });
+        .then(({ data }) => this.setState(() => ({
+          loggedin: true,
+        })))
+        .catch(({ error }) => this.setState(() => ({
+          loginerror: true,
+        })));
     };
 
+    // function returns conditional component based on graphql error return
+    errorRender() {
+      if (this.state.loginerror) {
+        return (
+          <Message
+            error
+            header="Unable to login"
+            content="Username or password is incorrect"
+          />
+        );
+      }
+      return <div />;
+    }
 
-  render() {
-    return (
-      <Grid columns={3}>
-        <Grid.Row />
-        <Grid.Row>
-          <Grid.Column />
+    render() {
+      if (this.state.loggedin === true) {
+        return <Redirect to="/lims" />;
+      }
+
+      return (
+        <Grid columns={3}>
+          <Grid.Row />
+          <Grid.Row>
+            <Grid.Column />
             <Grid.Column>
               <Container>
                 <Segment>
                   <Header size="huge">Please Login </Header>
-                  <Form onSubmit={this.handleSubmit}>
+                  <Form
+                    onSubmit={this.handleSubmit}
+                    error={this.state.loginerror}
+                  >
                     <Form.Field>
                       <Form.Input
                         fluid
@@ -38,6 +77,7 @@ class Login extends React.Component {
                         name="username"
                         value={this.state.username}
                         onChange={this.onChange}
+                        error={this.state.loginerror}
                       />
                     </Form.Field>
                     <Form.Field>
@@ -50,20 +90,23 @@ class Login extends React.Component {
                         name="password"
                         value={this.state.password}
                         onChange={this.onChange}
+                        error={this.state.loginerror}
                       />
                     </Form.Field>
                     <Button color="blue" fluid type="submit">Submit</Button>
                   </Form>
+                  { this.errorRender() }
                 </Segment>
               </Container>
             </Grid.Column>
-          <Grid.Column />
-        </Grid.Row>
-        <Grid.Row />
+            <Grid.Column />
+          </Grid.Row>
+          <Grid.Row />
 
-      </Grid>
-    );
-  }
+        </Grid>
+      );
+    }
 }
+
 
 export default Login;
