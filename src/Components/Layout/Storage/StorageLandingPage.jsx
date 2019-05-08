85211a912d019ee67cc58efd351c1d362d0a8fe3
Zach Smith <z4smith@ucsd.edu>
2019-05-08 16:48:09 -0700

    Adds landing page with listed/routed items

---
+++ src/Components/Layout/Storage/StorageLandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageMenu from '../PageMenu';
+
+
+export default class StorageSetup extends React.Component {
+  render() {
+    return (
+      <div>
+        <PageMenu />
+      </div>
+    );
+  }
+}
+
