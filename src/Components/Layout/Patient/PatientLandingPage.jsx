57bb932102c3e84fcafa2ddb33c3908db8bc58c5
Zach Smith <z4smith@ucsd.edu>
2019-05-30 14:02:33 -0700

    Moved patient table header into single file for ease of editing

--- src/Components/Layout/Patient/PatientLandingPage.jsx
+++ src/Components/Layout/Patient/PatientLandingPage.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
-import { Icon, Segment, Header, Dimmer, Loader, Table } from 'semantic-ui-react';
+import { Button, Icon, Segment, Header, Dimmer, Loader, Table } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import PageMenu from '../PageMenu';
 import { GET_ALL_PATIENTS } from '../../Data/PatientQueryData';
-import PatientTableHeader from './PatientLandingPageTableHeader';
 
+/*
+  Header values for the patient table for patient landing page
+
+  See backend model for all_patients (allPatients) for reference
+
+  While we could map over the values coming from graphql
+    1) Unwanted objects sneak in that are useful but not as table headers
+    2) It is currently overengineering and otherwise opaque
+    (table headers would be edited in schema)
+
+  If there are a large number of table changes that happen in the future then
+  mapping over the values to create the header should be revisited
+
+  Similiar to SpecimenLandingPageHeader
+*/
+
+const PatientTableHeader = () => (
+  <Table.Header>
+    <Table.Row>
+      <Table.HeaderCell>ID</Table.HeaderCell>
+      <Table.HeaderCell>PID</Table.HeaderCell>
+      <Table.HeaderCell>External ID</Table.HeaderCell>
+      <Table.HeaderCell>System Source</Table.HeaderCell>
+      <Table.HeaderCell>Synced</Table.HeaderCell>
+      <Table.HeaderCell>SyncDate</Table.HeaderCell>
+    </Table.Row>
+  </Table.Header>
+);
 
 class PatientLandingPage extends React.Component {
   buildTable = item => (
@@ -50,6 +77,9 @@ class PatientLandingPage extends React.Component {
             { this.props.patient.map(x => this.buildTable(x)) }
           </Table.Body>
         </Table>
+        <Segment basic>
+          <Button floated="right">Add Patient</Button>
+        </Segment>
       </div>
     );
   }
