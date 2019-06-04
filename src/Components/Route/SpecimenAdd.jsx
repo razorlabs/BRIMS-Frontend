26a2db387e88e3c5671cc694832742023520aad4
Zach Smith <z4smith@ucsd.edu>
2019-06-04 14:22:38 -0700

    Remove SpecimenAdd legacy component, moving to modal in specimen section

--- src/Components/Route/SpecimenAdd.jsx
+++
@@ -1,7 +0,0 @@
-import { graphql } from 'react-apollo';
-import { ADD_PATIENT } from '../Data/SpecimenDataMutations';
-import SpecimenEntry from '../Layout/Specimen/SpecimenEntry';
-
-const SpecimenAdd = graphql(ADD_PATIENT)(SpecimenEntry);
-
-export default SpecimenAdd;
