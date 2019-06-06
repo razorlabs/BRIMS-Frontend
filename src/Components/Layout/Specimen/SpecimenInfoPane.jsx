ae6e916a23583a32d675280c5ee0afad6d36492c
Zach Smith <z4smith@ucsd.edu>
2019-06-06 14:00:13 -0700

    Add outline for specimen add modal

--- src/Components/Layout/Specimen/SpecimenInfoPane.jsx
+++ src/Components/Layout/Specimen/SpecimenInfoPane.jsx
@@ -1,4 +1,5 @@
-import { Segment, Label, Container, Tab, Table, Loader, Dimmer } from 'semantic-ui-react';
+import { Modal, Form, Button, Segment, Label, Container, Tab, Table, Loader, Dimmer } from 'semantic-ui-react';
+import { DateInput, TimeInput } from 'semantic-ui-calendar-react';
 import PropTypes from 'prop-types';
 import React from 'react';
 import SpecimenTableHeader from './SpecimenTableHeader';
@@ -13,6 +14,94 @@ import AliquotInfoPaneWithData from '../../Route/AliquotInfoPaneWithData';
    panes differently when the number of specimen exceeds a certain
    amount making the display lengthy and cumbersome to navigate
 */
+
+class DatePickerInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event, { name, value }) => this.setState({ [name]: value })
+
+  render() {
+    return (
+      <Form>
+        <DateInput
+          name="date"
+          clearable
+          closable
+          placeholder="Date"
+          value={this.state.date}
+          iconPosition="right"
+          onChange={this.handleChange}
+          dateFormat="YYYY-MMM-DD"
+        />
+      </Form>
+    );
+  }
+}
+
+class TimePickerInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event, { name, value }) => this.setState({ [name]: value })
+
+  render() {
+    return (
+      <Form>
+        <TimeInput
+          clearable
+          closable
+          name="time"
+          placeholder="Time"
+          value={this.state.time}
+          iconPosition="right"
+          onChange={this.handleChange}
+        />
+      </Form>
+    );
+  }
+}
+
+const AddSpecimenModal = () => (
+  <Modal trigger={<Button floated="right">Add Specimen</Button>}>
+    <Modal.Header>Add Specimen</Modal.Header>
+    <Modal.Content>
+      <Form>
+        <Form.Field>
+          <label>Type</label>
+          <input placeholder="Type" />
+        </Form.Field>
+        <Form.Field>
+          <label>Volume</label>
+          <input placeholder="Type" />
+        </Form.Field>
+        <Form.Field>
+          <label>Collect Date</label>
+          <DatePickerInput />
+        </Form.Field>
+        <Form.Field>
+          <label>Collect Time</label>
+          <TimePickerInput />
+        </Form.Field>
+        <Form.Field>
+          <label>Notes</label>
+          <input placeholder="Type" />
+        </Form.Field>
+      </Form>
+    </Modal.Content>
+  </Modal>
+);
+
 
 function BuildPanes(type, id, specimen) {
   const item = type.concat(' ID:', id);
@@ -58,6 +147,9 @@ class SpecimenInfoPane extends React.Component {
             <Table celled>
               <Tab panes={panes} />
             </Table>
+            <AddSpecimenModal />
+            <br />
+            <br />
           </Segment>
         </Segment.Group>
       </Container>
