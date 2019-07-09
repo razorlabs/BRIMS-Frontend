6dd6bb12ed5d6812fe3517cf46071e2bf26ed5a8
Zach Smith <z4smith@ucsd.edu>
2019-07-09 10:56:22 -0700

    Adds "AddSpecimenModal" which contains modal logic for adding specimen from the specimen info pane

---
+++ src/Components/Layout/Specimen/AddSpecimenModal.jsx
@@ -0,0 +1,139 @@
+import { Dropdown, Modal, Form, Button } from 'semantic-ui-react';
+import React from 'react';
+import { DateInput, TimeInput } from 'semantic-ui-calendar-react';
+
+import gql from 'graphql-tag';
+import { graphql, compose } from 'react-apollo';
+import { ADD_SPECIMEN } from '../../Data/SpecimenMutations';
+import { GET_SPECIMEN_TYPES } from '../../Data/SpecimenQueryData';
+
+/*
+  The Add Specimen Modal inherents the target patient via a props pass and
+  composes two graphql calls, one for querying types of specimen as part of
+  the type requirement, and the second carrying the mutation call for db
+  commit of new specimen on the target patient.
+*/
+
+class AddSpecimenModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: '',
+      volume: '',
+      date: '',
+      time: '',
+      notes: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    console.log(this.props);
+  }
+
+  handleChange = (event, { name, value }) => this.setState({ [name]: value })
+
+  handleSubmit = () => {
+    console.log(this.props);
+    console.log(this.state);
+    /* this.props.mutate({
+      variables: {
+        patient: 1,
+        specimentype: 'Whole Blood',
+        volume: 10,
+        collectDate: '2019-07-07',
+        collectTime: '23:19:03',
+        notes: 'Added from modal',
+      },
+    }); */
+  }
+
+  render() {
+    /*
+      create list of objects displaying options for specimen from
+      GET_SPECIMEN_TYPES query
+    */
+    let listdict;
+    if (this.props.loading) {
+      listdict = 'Loading...';
+    } else {
+      listdict = this.props.alltypes.map(x => ({ key: x.type, value: x.id, text: x.type }));
+    }
+    return (
+      <Modal trigger={<Button floated="right">Add Specimen</Button>}>
+        <Modal.Header>Add Specimen</Modal.Header>
+        <Modal.Content>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Field>
+              <label>Specimen Type</label>
+              <Dropdown
+                name="type"
+                placeholder="Specimen Type"
+                selection
+                search
+                options={listdict}
+                onChange={this.handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Volume</label>
+              <Form.Input placeholder="Volume" name="volume" value={this.state.volume} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <label>Collect Date</label>
+              <Form>
+                <DateInput
+                  name="date"
+                  clearable
+                  closable
+                  placeholder="Date"
+                  value={this.state.date}
+                  iconPosition="right"
+                  onChange={this.handleChange}
+                  dateFormat="YYYY-MMM-DD"
+                />
+              </Form>
+            </Form.Field>
+            <Form.Field>
+              <label>Collect Time</label>
+              <Form>
+                <TimeInput
+                  clearable
+                  closable
+                  name="time"
+                  placeholder="Time"
+                  value={this.state.time}
+                  iconPosition="right"
+                  onChange={this.handleChange}
+                />
+              </Form>
+            </Form.Field>
+            <Form.Field>
+              <label>Notes</label>
+              <Form.Input placeholder="Notes" name="notes" value={this.state.notes} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Button floated="right" content="Submit" />
+            <Form.Field />
+          </Form>
+        </Modal.Content>
+      </Modal>
+    )
+  }
+}
+
+const SpecimenPickerData = graphql(GET_SPECIMEN_TYPES, {
+  props: ({ data }) => ({
+    error: data.error,
+    loading: data.loading,
+    alltypes: data.allSpecimenTypes,
+  }),
+});
+
+const AddSpecimen = graphql(ADD_SPECIMEN, {
+  options: props => ({
+    variables: {
+      patient: props.patientid,
+    },
+  }),
+});
+
+const AddSpecimenModalMutation = compose(AddSpecimen, SpecimenPickerData)(AddSpecimenModal);
+
+export default AddSpecimenModalMutation;
