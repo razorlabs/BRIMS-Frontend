0cfab2e9500859837d473b207892948041c7e799
Zach Smith <z4smith@ucsd.edu>
2019-08-08 10:33:26 -0700

    Adds draw schedule (required for knowing specimen to draw) to patient mutation

--- src/Components/Data/PatientMutation.jsx
+++ src/Components/Data/PatientMutation.jsx
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export const ADD_PATIENT = gql`
-  mutation createPatient($pid: String!, $externalId: String!) {
-    createPatient(pid: $pid, externalId: $externalId) {
+  mutation createPatient($pid: String!, $externalId: String!, $drawschedule: Int!) {
+    createPatient(pid: $pid, externalId: $externalId, drawschedule: $drawschedule) {
       id
       pid
       externalId
