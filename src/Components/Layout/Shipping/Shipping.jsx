abc948c82e6fb39b28250d99661ef8f86e5a6728
Zach Smith <z4smith@ucsd.edu>
2020-03-05 13:57:50 -0800

    Complete revamp of shipping view

--- src/Components/Layout/Shipping/Shipping.jsx
+++ src/Components/Layout/Shipping/Shipping.jsx
@@ -1,33 +1,58 @@
-import { Tab, Container } from 'semantic-ui-react';
+import { Table, Dimmer, Loader, Container } from 'semantic-ui-react';
 import React from 'react';
+import { graphql } from 'react-apollo';
 import PageMenu from '../PageMenu';
-import PendingReceivingWithData from './PendingReceiving';
-import PendingShippingWithData from './PendingShipment';
+import ShippingInfoRow from './ShippingInfoRow';
+import { GET_SHIPMENTS } from '../../Data/ShippingQueryData';
 
 
-class ShippingCombined extends React.Component {
+const ShipmentHeader = () => (
+  <Table.Header>
+    <Table.Row>
+      <Table.HeaderCell>ID</Table.HeaderCell>
+      <Table.HeaderCell>Manifest</Table.HeaderCell>
+      <Table.HeaderCell>Carrier</Table.HeaderCell>
+      <Table.HeaderCell>Shipment Number</Table.HeaderCell>
+      <Table.HeaderCell>Destination</Table.HeaderCell>
+      <Table.HeaderCell>Sent Date</Table.HeaderCell>
+      <Table.HeaderCell>Received Date</Table.HeaderCell>
+      <Table.HeaderCell>Ship Now</Table.HeaderCell>
+    </Table.Row>
+  </Table.Header>
+);
+
+
+class ShippingUI extends React.Component {
   render() {
-    const panes = [
-      {
-        menuItem: 'Pending Shipment',
-        render: () => <Tab.Pane> <PendingShippingWithData /> </Tab.Pane>,
-      },
-      {
-        menuItem: 'Pending Receiving',
-        render: () => <Tab.Pane> <PendingReceivingWithData /> </Tab.Pane>,
-      },
-    ];
+    if (this.props.data.loading) {
+      return <Dimmer active> <Loader /> </Dimmer>;
+    }
+    if (this.props.data.error) {
+      return <p> Error! </p>;
+    }
+    console.log(this.props.data.allShipments);
     return (
       <div>
-        <PageMenu />
-        <Container>
-          <Tab panes={panes} />
-        </Container>
+          <PageMenu />
+          <Container>
+            <Table padded celled>
+              <ShipmentHeader />
+              {this.props.data.allShipments.map((shipping) => {
+                return <ShippingInfoRow shipping={shipping} key={shipping.id} />
+              })}
+            </Table>
+          </Container>
       </div>
     );
   }
 }
 
-const Shipping = ShippingCombined;
+/*
+          {this.props.data.allShipments.map((shipping) => {
+            return <PendingShipmentInfoRow shipping={shipping} key={shipping.id} />;
+          })}
+*/
+
+const Shipping = graphql(GET_SHIPMENTS)(ShippingUI)
 
 export default Shipping;
